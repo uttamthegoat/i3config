c2ed4e4db81e323fb0f027de18cfa42068def15f
Uttam Prabhu <19516uttam@gmail.com>
2025-09-03 20:29:05 +0530

    (feat): added application launcher, file manager, text editor

--- update_config.sh
+++ update_config.sh
@@ -1,2 +1,4 @@
 cp -r ~/.config/i3/. ~/Code/i3config/i3/
 cp -r ~/.config/i3blocks/. ~/Code/i3config/i3blocks/
+cp -r ~/.config/dunst/. ~/Code/i3config/dunst/
+cp -r ~/.config/rofi/. ~/Code/i3config/rofi/
