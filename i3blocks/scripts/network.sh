e5e77324379037e06cd9f97dccef3a985087b478
Uttam Prabhu <19516uttam@gmail.com>
2025-09-11 01:13:31 +0530

    (feat): added blur(picom), changed terminal logo

--- i3blocks/scripts/network.sh
+++ i3blocks/scripts/network.sh
@@ -2,4 +2,4 @@
 # Output the current Wi-Fi SSID or "Not Connected"
 echo "$(nmcli -t -f ACTIVE,SSID dev wifi | grep '^yes' | cut -d: -f2 || echo 'Not Connected')"
 # Launch nmtui in kitty when clicked
-[[ -z "${BLOCK_BUTTON}" ]] || (export DISPLAY=:0; kitty -e nmtui &)
+[[ -z "${BLOCK_BUTTON}" ]] || (export DISPLAY=:0; export TERM=xterm-256color; kitty --config ~/.config/kitty/network.conf -e nmtui &)
